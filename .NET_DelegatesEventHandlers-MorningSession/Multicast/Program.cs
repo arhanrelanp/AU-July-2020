ff1561c777a359f681a0712a2c440a967651ac18
Arhan <arhan.relan@accoliteindia.com>
2020-08-07 04:08:29 +0530

    Revert "Assignment Updated"
    This reverts commit 28eac17c4aeaff962a1915851f0952d1007bf2b7.

--- .NET_DelegatesEventHandlers-MorningSession/Multicast/Program.cs
+++ .NET_DelegatesEventHandlers-MorningSession/Multicast/Program.cs
@@ -3,25 +3,50 @@
 
 namespace DelegatesEventHandlers
 {
-    
+    class Multicast
+    {
+        public delegate void Concat(string s1, string s2);
+        private string result;
+        public Multicast()
+        {
+            result = "";
+        }
+        public void f1(string s1,string s2)
+        {
+            if (s1.Length > s2.Length)
+                result = result + s1;
+            else
+                result = result + s2;
+        }
+        public void f2(string s1, string s2)
+        {
+            if (s1.Length > s2.Length)
+                result = result + s1;
+            else
+                result = result + s2;
+        }
+        public void StrConcat()
+        {
+            string a, b;
+            Concat c;
+            c = f1;
+            c += f2;
+            Console.WriteLine("String 1: ");
+            a = Console.ReadLine();
+            Console.WriteLine("String 2: ");
+            b = Console.ReadLine();
+            c(a, b);
+            Console.WriteLine("Value of concatenated longer string is: " + result);
+
+        }
+    }
     class Program
     {
-        public delegate int count(string s1, string s2);
-        public static int countChars(string s1, string s2)
-        {
-            string res = s1 + s2;
-            return res.Length;
-        }
+     
         static void Main(string[] args)
         {
-            count c = new count(countChars);
-            Console.WriteLine("String 1: ");
-            string a = Console.ReadLine();
-            Console.WriteLine("String 2: ");
-            string b = Console.ReadLine();
-            Console.WriteLine(a + "" + b);
-            Console.WriteLine("Length: " + c(a, b));
-
+            Multicast m = new Multicast();
+            m.StrConcat();
         }
     }
 }
