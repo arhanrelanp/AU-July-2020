56f2a9e73ccbd649d016c1e29427f3ee972804ee
Arhan <arhan.relan@accoliteindia.com>
2020-08-07 09:09:07 +0530

    Reverted Deletion

---
+++ .NET_DelegatesEventHandlers-MorningSession/Leaves/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Leaves
+{
+    public delegate void Notify();              // delegate
+    public event Notify ProcessCompleted;       // event
+    public int NoOfLeaves;
+    public int AvailLeaves;
+
+    public Leaves(int lr, int la)
+    {
+        NoOfLeaves = lr;
+        AvailLeaves = la;
+    }
+
+
+    public void verify()
+    {
+        Console.WriteLine("Application process started!");
+        if (AvailLeaves >= NoOfLeaves)
+        {
+            ProcessCompleted?.Invoke();
+        }
+        else
+        {
+            Console.WriteLine("Leaves not available.");
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Number of leaves required .");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number of available leaves .");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Leaves app = new Leaves(l, a);
+            app.ProcessCompleted += ApplChecked; // register with an event
+            app.verify();
+        }
+
+        // event handler
+        public static void ApplChecked()
+        {
+            Console.WriteLine("Confirm please(Y/N)");
+            String resp = Console.ReadLine();
+            if (resp.Equals("Y"))
+            {
+                Console.WriteLine("Leave application submitted to manager!!");
+            }
+            else
+            {
+                Console.WriteLine("Leave application cancelled");
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+}
---
+++ .NET_DelegatesEventHandlers-MorningSession/NoofChars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DelegatesEventHandlers
+{
+
+    public class Program
+    {
+
+        public delegate int count(string s1, string s2);
+
+        public static int countChars(string s1, string s2)
+        {
+            String res = s1 + s2;
+            return res.Length;
+        }
+
+        static void Main(string[] args)
+        {
+            count c = new count(countChars);
+            Console.WriteLine("String 1: ");
+            String a = Console.ReadLine();
+            Console.WriteLine("String 2: ");
+            String b = Console.ReadLine();
+            Console.WriteLine(a + " " + b);
+            Console.WriteLine(c(a, b));
+
+
+
+        }
+    }
+}
+
+
+
+
